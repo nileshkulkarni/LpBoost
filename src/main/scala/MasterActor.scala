749b62a9975fcd4bed6ba7747536b17f512019f5
Nilesh <nileshsatishkulkarni@gmail.com>
2015-04-30 19:22:31 +0530

    some issue with getting instances from the array

--- src/main/scala/MasterActor.scala
+++ src/main/scala/MasterActor.scala
@@ -1,12 +1,16 @@
 import akka.actor._
 import akka.actor.ActorRef
 import akka.actor.Actor
+
 import Array._
+
+import collection.mutable._
 
 case class startLpBoost() // This is the message recieved to start the process
 case class updatedWorkerVariables(id: Integer,wK: Array[Double], betaK: Double,phi:Array[Double] ,lambda:Double)
+case class doneProcessing(status: Integer)
 
-class MasterActor(filename: String) extends Actor {
+class MasterActor() extends Actor {
 
     private var runningLpBoost = false
     private var totalLines = 0
@@ -15,12 +19,27 @@ class MasterActor(filename: String) extends Actor {
     private var fileSender: Option[ActorRef] = None
     private var noOfPartitions = 2
     private var noOfExamples = 4
+    
+    
     private var weights = ofDim[Double](noOfPartitions,noOfExamples) 
     private var betas = ofDim[Double](noOfPartitions) 
     private var lambdas = ofDim[Double](noOfPartitions) 
     private var phis = ofDim[Double](noOfPartitions,noOfExamples) 
-    private var phi = ofDim[Double](noOfExamples)
-    private var beta=0.0
+    
+
+    private var con_weightsM = ofDim[Double](noOfExamples) 
+    private var betaM=0.0
+    
+    private var furtherUpdates = ofDim[Boolean](noOfPartitions)
+
+    private var fileName="NULL"
+
+    private var temp: Int = 1
+    def this( fileName:String) ={
+        this();
+        this.fileName = fileName
+        //intialize beta
+    }
     def receive = {
         case startLpBoost() => {
             if (runningLpBoost) {
@@ -30,12 +49,15 @@ class MasterActor(filename: String) extends Actor {
             } else {
                 runningLpBoost = true
                 fileSender = Some(sender) // save reference to process invoker
+                sender ! doneProcessing(1)
                 //create actors
-                creatActors()
+                createActors()
+                updateBetaAndLambda()
             }
         }
         case updatedWorkerVariables(id,wK,betaK,phi,lambda) => {
             println("Received response for weights and beta")
+            furtherUpdates(id) = false
             //for(a<- 0 untill noOfExamples){
             weights(id) = wK
             betas(id) = betaK;
@@ -45,23 +67,50 @@ class MasterActor(filename: String) extends Actor {
         }
         case _ => println("message not recognized!")
     }
-    def creatActors(){
+    def createActors(){
        for(a <-0 until noOfPartitions){
-            ActorFactory.addActor(a,context.actorOf(Props(new WorkerActor(a,filename)))) 
+            furtherUpdates(a) = false
+       }
+
+       for(a <-0 until noOfPartitions){
+            ActorFactory.addActor(a,context.actorOf(Props(new WorkerActor(a,fileName)))) 
        }
     }
-    def checkIfMoreUpdatesRequired(){
-        return false
+
+    def checkIfMoreUpdatesRequired(): Boolean={
+        var updatesReq :Boolean =false
+        for(a<-0 until noOfPartitions){
+            updatesReq = furtherUpdates(a) || updatesReq
+        }
+        return updatesReq
     }
-    def updateBetaAndLambda = {
+
+    def checkForStoppingCriterion(): Boolean={
+        if(temp==1){
+            temp =0
+            return false
+        }
+        else
+            return true
+    }
+    def updateBetaAndLambda() {
         // compute Beta and lambda and call the update Weights function
             
-            val furtherUpdates = checkIfMoreUpdatesRequired()
-            if(furtherUpdates==true){
-                for( a <- 0 until noOfPartitions){
-                    val actorWorker = ActorFactory.getActor(a)
-                    actorWorker ! updateWeightsBetaLambdaPhi(filename,weights,beta)
+
+            if(!checkIfMoreUpdatesRequired()){
+                // Check if stopping criterion is met
+                if(checkForStoppingCriterion()){
+                    println("Stopping Criterion met.")
+                    return
                 }
+                println("here noOfPartitions is " + noOfPartitions);
+                println("File Name " + fileName)
+                for( a <- 0 until noOfPartitions){
+                    var actorWorker: ActorRef = ActorFactory.getActor(a)
+                    println("Sending beta and lambda updates to actor " + a);
+                    actorWorker ! updateWeightsBetaLambdaPhi(fileName,con_weightsM,betaM)
+                }
+                println("here1");
             }
     }
     def updateLambdaKBetaK(id:Integer,phi:Array[Double],lambda:Double)={
@@ -69,13 +118,20 @@ class MasterActor(filename: String) extends Actor {
             phis(id) = phi
     }
 }
+
 object ActorFactory {
-    val actorMap = new HashMap[Intger,Actor] with SynchronizedMap[Integer,Actor]
-    def addActor(key: Integer,myActor: Actor)={
-        actorMap += (key,myActor) 
+    val actorMap = new HashMap[Integer,ActorRef] with SynchronizedMap[Integer,ActorRef]
+    def addActor(key: Integer,myActor: ActorRef)={
+        actorMap.put(key,myActor) 
     }
-    def getActor(symbol:Integer): Actor = {
-        val actor = actorMap.get(symbol)
-        return actor
+    def getActor(symbol:Integer): ActorRef = {
+        println("getActor : called with symbol " + symbol)
+        val wactor : ActorRef = actorMap.get(symbol).asInstanceOf[ActorRef]
+        //val wactor : ActorRef = actorMap.get(symbol)
+        println("Returning actor: "+ wactor)
+        for ((k,v) <- actorMap) { 
+            println("Key : " + k  + " actorRef  "+ v)
+        }        // k is the key, v is the value
+        return wactor
     }
 }
--- src/main/scala/WorkerActor.scala
+++ src/main/scala/WorkerActor.scala
@@ -1,13 +1,30 @@
 import akka.actor._
 
+import Array._
+import collection.mutable._
+
 case class ProcessStringMsg(string: String)
 case class StringProcessedMsg(words: Integer)
-case class updateWeightsBeta(fileName:String, weight: Array[Double], beta:Double)
-class WorkerActor (id: Integer, filename: String) extends Actor {
+case class updateWeightsBetaLambdaPhi(fileName:String, weight: Array[Double], beta:Double)
+class WorkerActor extends Actor {
+    private var id =0
+    private var filename="NULL File"
+    private var noOfExamples = 4
+    private var beta=0.0
+    private var lambda = 0.0 
+    private var phi = ofDim[Double](noOfExamples) 
+    private var weight = ofDim[Double](noOfExamples) 
+    def this(value: Integer, filename:String) ={
+        this();
+        this.id = value
+        this.filename = filename
+        println("Spwaned Worker Actor with id " + value)
+
+    }
     def receive = {
         case updateWeightsBetaLambdaPhi (fileName: String, weight: Array[Double], beta: Double) => {
-            println("File Name " + fileName); 
-            sender ! updatedWorkerVariables()
+            println("File Name " + fileName)
+            sender ! updatedWorkerVariables(id,weight,beta,phi,lambda)
         }
         case _ => println("Error: Worker Actor: message not recognized")
     }
